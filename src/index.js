1ffd8398361d0d12eacaeafed0d1948e4bb061f5
Miguel Gonzalez <miguelgm.priv@gmail.com>
2021-10-03 13:39:25 -0700

    Add menu links using js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,36 @@
 import './styles.css';
-import background from './background.jpg';
-
-
-console.log('hello')
-console.log('test')
 
 const mainContainer = document.createElement('div')
 mainContainer.classList.add('container');
 document.body.appendChild(mainContainer);
+
+
+const pageHeader = (() => {
+    const linkArray = ['home','menu','contact']
+    const header = document.createElement('div');
+    const navBar = document.createElement('ul');
+
+    linkArray.forEach(element => {
+        const li = document.createElement('li');
+        li.textContent = element[0].toUpperCase() + element.slice(1);
+        li.id = element;
+        navBar.appendChild(li);
+    })
+/*     const homeLink = document.createElement('li');
+    const menuLink = document.createElement('li');
+    const contactLink = document.createElement('li');
+ */
+    header.classList.add('header');
+
+    header.textContent = 'The Hungry Cat Cafe';
+/*     homeLink.textContent = 'Home';
+    menuLink.textContent = 'Menu';
+    contactLink.textContent = 'Contact';
+
+    navBar.appendChild(homeLink);
+    navBar.appendChild(menuLink);
+    navBar.appendChild(contactLink); */
+
+    mainContainer.appendChild(header);
+    header.appendChild(navBar);
+})();
