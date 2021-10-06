0cd29163c3fb3676862c6696b2f461e6d665c6ba
Miguel Gonzalez <miguelgm.priv@gmail.com>
2021-10-05 18:41:14 -0700

    Move header and footer to separate module added footerBarLink class in the event of having to add more recognitions

--- src/header-footer.js
+++ src/header-footer.js
@@ -1,5 +1,5 @@
 const pageTitle = 'The Hungry Cat Cafe';
-const linkArray = ['home', 'menu', 'contact']
+const navLinks = ['home', 'menu', 'contact']
 
 const header = document.createElement('div');
 header.classList.add('header');
@@ -8,15 +8,48 @@ header.textContent = pageTitle;
 const navBar = document.createElement('ul');
 navBar.classList.add('navbar');
 
-linkArray.forEach(element => {
+navLinks.forEach(element => {
     const li = document.createElement('li');
     li.textContent = element[0].toUpperCase() + element.slice(1);
     li.id = element;
     navBar.appendChild(li);
 });
+
 const pageHeader = ((element) => {
     element.appendChild(header);
     header.appendChild(navBar);
 });
 
-export default pageHeader;
+
+const footer = document.createElement('div');
+const footerBar = document.createElement('ul');
+
+footer.classList.add('footer');
+
+class footerBarLink {
+    constructor(text, url){
+        this.text = text;
+        this.url = url;
+    }
+}
+let footerLinks = [];
+footerLinks[0] = new footerBarLink('Food photo created by timolina - www.freepik.com', 'https://www.freepik.com/photos/food');
+
+footerLinks.forEach(element => {
+    const a = document.createElement('a');
+    a.title = element.text;
+    a.href = element.url;
+
+    const li = document.createElement('li');
+    li.textContent = element.text;
+    
+    a.appendChild(li);
+    footerBar.appendChild(a);
+});
+
+const pageFooter = ((element) => {
+    footer.appendChild(footerBar);
+    element.appendChild(footer)
+});
+
+export { pageHeader , pageFooter, navLinks };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './styles.css';
-import pageHeader from './header-footer.js';
+import { pageHeader, pageFooter, navLinks } from './header-footer.js';
 
 const mainContainer = document.createElement('div')
 mainContainer.classList.add('container');
@@ -13,17 +13,5 @@ const pageContent = (() => {
     content.textContent = 'Leave the whiskas at home and come enjoy our decadent fancy feast prepared by our team of world class chefs!';
     mainContainer.appendChild(content);
 })();
-const pageFooter = (() => {
-    const footer = document.createElement('div');
-    footer.classList.add('footer');
-    const creditsLinks = document.createElement('ul');
-    const a = document.createElement('a');
-    a.title = 'https://www.freepik.com/photos/food';
-    a.href = 'https://www.freepik.com/photos/food';
-    const li = document.createElement('li')
-    li.textContent = 'Food photo created by timolina - www.freepik.com';
-    a.appendChild(li);
-    creditsLinks.appendChild(a);
-    footer.appendChild(creditsLinks);
-    mainContainer.appendChild(footer)
-})();
+
+pageFooter(mainContainer);
